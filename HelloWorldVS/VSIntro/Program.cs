3dcc0b5964f3d8fa87b1f69255bdafaead84bb41
Madara Rorbaha <madararorbaha@gmail.com>
2022-11-21 17:33:11 +0200

    14

--- HelloWorldVS/VSIntro/Program.cs
+++ HelloWorldVS/VSIntro/Program.cs
@@ -1,10 +1,33 @@
 ﻿
 class VSIntro
+    /////Defined by Madara on 10th of November
+    /////Changed on 14th of Nov
 {
   public static void Main(string[] args)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World 2!");
-        Console.WriteLine("Hello, World 3!");
+        //int intvariable = 20;
+        ////// See https://aka.ms/new-console-template for more information
+        ////Console.WriteLine("Hello, World 2!");
+        ////Console.WriteLine("Hello, World 3!");
+        //Console.WriteLine(intvariable);
+        //intvariable = 18;
+        //Console.WriteLine(intvariable);
+        //intvariable= 27;
+        //Console.WriteLine(intvariable);
+
+        ////Left////////////////////////Center//////////////Right
+        string name = "John";
+        uint age = 18;///unsigned integer, because the age cannot be negative
+        //int age = 18;/// integer
+        string strNo = "18";
+        char gender = 'M';
+        float heigh = 1.87F;
+        bool underage, smoking;
+        //Console.WriteLine(name);
+        //Console.WriteLine(age);
+        //Console.WriteLine(gender);
+        //Console.WriteLine(heigh);
+        //Console.WriteLine(name + " " + age + " " + gender + " " + heigh);
+        Console.WriteLine(name + '\n' + age + '\n' + gender + '\n' + heigh);
     }
 }
---
+++ Practical1/Practical1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+
+class Practical1
+{
+    static void Main(string[] args)
+    {
+
+        //int valInt = 7;
+        //double valDob = valInt;// casting done automatically, because there is no
+        ////ptencial data lose
+        //Console.WriteLine(valDob);
+        //valDob= 10.56;
+        //valInt= (int) valDob;//casting done manually, because there is 
+        ////potencial data lose
+        //Console.WriteLine(valInt);
+        //char charVar = 'A';
+        //valInt = (int) charVar;
+        //Console.WriteLine(valInt);
+
+        //char unicodeVar = '\u0023';
+        //Console.WriteLine(unicodeVar);
+
+        //string str = "\u0023";//"\\u0023" extra \ also escapes
+        //Console.WriteLine(str);
+        //string StrAt = @"\u0023";
+        //Console.WriteLine(StrAt);
+
+        //string str1 = "C:\\Users\\madar\\OneDrive\\Documents\\dotnetPractical\\Practical1";//need to use the escape characters 
+        //string str2 = @"C:\Users\madar\OneDrive\Documents\dotnetPractical\Practical1";//do not need to use the escape characters
+
+
+        //Console.WriteLine("Please enter something:");
+        //string input = Console.ReadLine();//from VS to Console
+        //Console.WriteLine("Please enter something again:");
+        //string input2 = Console.ReadLine();
+        //string result = input + input2;
+        //Console.WriteLine(result);
+
+        // Console.WriteLine("You entered : " + input);
+        //Console.WriteLine("You entered : {0}", input);
+        Console.WriteLine("Enter number");
+        string number1str = Console.ReadLine();
+        string number2str = Console.ReadLine();
+        int number1 = Int32.Parse(number1str);
+        int number2 = Int32.Parse(number2str);
+        int result = number1 + number2;
+        Console.WriteLine(result);
+    }
+}
